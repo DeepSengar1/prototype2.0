a92f790d079c36f024a16f73d71a86c9d003b62f
Deep Sengar <128704275+DeepSengar1@users.noreply.github.com>
2025-04-16 15:34:43 +0530

    fixed routing

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import Layout from "./pages/layout";
-import { Outlet, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import Landingpage from "./pages/LandingPage/Landingpage";
 
 const App = () => {
   return (
     <>
       <Layout>
         <Routes>
-          <Route path="/" element={<Landingpage />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </Layout>
     </>
