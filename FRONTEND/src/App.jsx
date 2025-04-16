50f70c58d9ce61206cce1d0469b512462d466e8e
avinashmadnani05 <81864893+avinashmadnani05@users.noreply.github.com>
2025-04-16 15:36:07 +0530

    xc
    c

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,7 +1,20 @@
 import Ai from "./Ai";
+import Simuate from "./Simulate";
+import VoiceBot from "./VoiceBot";
 
 function App() {
-  return <><Ai/></>;
-}
+
+  return( 
+  <>
+  <Ai/> 
+  <h1>Hello</h1>
+  <VoiceBot />
+  <Simuate />
+  
+  </> 
+
+
+  );
+};
 
 export default App;
