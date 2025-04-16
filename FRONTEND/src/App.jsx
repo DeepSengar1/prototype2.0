483565c404c9d882ccfa4ded4cfb5a8aa925208a
Deep Sengar <128704275+DeepSengar1@users.noreply.github.com>
2025-04-16 15:31:50 +0530

    added heatmap

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,7 +1,20 @@
-import Ai from "./Ai";
+import React from "react";
+import Layout from "./pages/layout";
+import { Outlet, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Landingpage from "./pages/LandingPage/Landingpage";
 
-function App() {
-  return <><Ai/></>;
-}
+const App = () => {
+  return (
+    <>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Landingpage />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Layout>
+    </>
+  );
+};
 
 export default App;
